a55100ce5c9a8ddd3f3a2ca83ead5c69b771e51c
Rohan Sidhu <rohan.sidhu@outlook.com>
2023-08-22 20:38:48 +0530

    HomeBanner Has Been Created

--- index.php
+++ index.php
@@ -7,7 +7,25 @@
     <?php include_once("./core/header.php"); ?>
 </head>
 <body>
-    
+<div id='wrapper'>
+    <section id="homeBanner">
+        <div class='flexContainer'>
+            <div id='flex' style='flex: 1;'>
+                <h1>I'm WLuxhomes</h1>
+                <p>WLuxhomes is a real estate company that focuses on serving <b>women by women.</b></p><br>
+                <button>Read More...</button>
+            </div>
+            <div id='flex' style='flex: 1;'>
+                <center><img src='./resource/img/businessWomen.png'></center>
+            </div>
+        </div>
+    </section>
+    <section id="aboutUs">
+        <div id="titleArea">
+            <center><h1>About Us</h1></center>
+        </div>
+    </section>
+</div>
     <?php include_once("./core/header.php"); ?>
 </body>
 </html>
