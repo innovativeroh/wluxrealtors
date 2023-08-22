b784e127c8e7d9186b552e8e5211377db7f91cda
Rohan Sidhu <rohan.sidhu@outlook.com>
2023-08-22 22:01:17 +0530

    Services Section Has Been Completed

--- index.php
+++ index.php
@@ -22,10 +22,60 @@
     </section>
     <section id="aboutUs">
         <div id="titleArea">
-            <center><h1>About Us</h1></center>
+            <center>
+                <h1>About Us</h1>
+                <hr>
+            </center>
+        </div>
+        <br><br>
+        <div class='flexContainer' style='gap: 150px'>
+            <div id='flex' style='flex: 2.5;'>
+                <h2>Introducing<br>WLuxhomes:<br>Empowering Women in<br>Real Estate</h2>
+                <hr><br>
+                <p style="font-size: 16px; line-height: 25px;">WLuxhomes is a real estate company dedicated to serving the needs of women. We understand the unique challenges and preferences that women face when it comes to buying or selling property. Our team of experienced professionals is committed to providing exceptional service and guidance throughout the entire real estate process. With a focus on integrity, professionalism, and attention to detail, WLuxhomes is here to help women achieve their real estate goals with confidence and ease.</p><br><br>
+                <button>Contact Us</button>
+            </div>
+            <div id='flex' style='flex: 1;'>
+                <img src='./resource/img/img1.webp' style='width: 350px; border: 12px solid var(--color-secondary); border-radius: 8px;'>
+
+            </div>
+        </div>
+    </section>
+    <section id="provideService">
+    <div id="titleArea">
+            <center>
+                <h1>What Services We're Providing</h1>
+                <hr>
+            </center>
+        </div>
+        <br><br><br><br>
+        <div class="flexBoxes">
+            <div id='flex' style='flex: 1;'>
+            <img src='./resource/img/img2.webp' style='width: 100%; margin-bottom: 15px;'>
+            <h1 style='padding-bottom: 22px;'>Tailored Home Search for Women Buyers<h1>
+            <p style="font-size: 16px; line-height: 25px; color: var(--color-light-black); font-weight: 400;">I am dedicated to providing personalized home searches for women, tailored to their unique needs and preferences.</p>
+            </div>
+            <div id='flex' style='flex: 1;'>
+            <img src='./resource/img/img3.webp' style='width: 100%; margin-bottom: 15px;'>
+            <h1 style='padding-bottom: 22px;'>Empowering Women in Real Estate Sales<h1>
+            <p style="font-size: 16px; line-height: 25px; color: var(--color-light-black); font-weight: 400;">I specialize in selling your property with expertise and professionalism, ensuring a smooth and successful transaction.</p>
+            </div>
+            <div id='flex' style='flex: 1;'>
+            <img src='./resource/img/img4.webp' style='width: 100%; margin-bottom: 15px;'>
+            <h1 style='padding-bottom: 22px;'>Empowering Women in Real Estate Investments<h1>
+            <p style="font-size: 16px; line-height: 25px; color: var(--color-light-black); font-weight: 400;">I am WLuxhomes, a real estate investment advisory service specifically designed for women, providing professional guidance and expertise.</p>
+            </div>
         </div>
     </section>
 </div>
+<section id="workExperience">
+    <div id="titleArea">
+            <center>
+                <h1>Work Experience</h1>
+                <hr>
+            </center>
+        </div>
+</section>
     <?php include_once("./core/header.php"); ?>
 </body>
 </html>
